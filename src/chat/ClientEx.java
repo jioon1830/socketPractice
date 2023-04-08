6b212a719d0296942ac8d4438ff72f4a76affe2d
정지운 <jioon@jeongjiun-ui-MacBookAir.local>
2023-04-08 21:52:40 +0900

    socketPrac

--- src/chat/ClientEx.java
+++ src/chat/ClientEx.java
@@ -5,7 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ServerEx {
+public class ClientEx {
     public static void main(String[] args){
         ServerSocket server = null;
         Socket socket = null;
@@ -13,26 +13,25 @@ public class ServerEx {
         BufferedWriter out = null;
         Scanner sc = new Scanner(System.in);
         try {
-            server = new ServerSocket(9999);
-            System.out.println("Waiting Connection..");
-            socket = server.accept();
-            System.out.println("Connected");
+            socket = new Socket("localhost", 9999);
+
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
             while(true){
-                String inMsg = in.readLine();
-                if(inMsg.equalsIgnoreCase("quit")){
+                System.out.print("Send: ");
+                String outMsg = sc.nextLine();
+
+                if(outMsg.equalsIgnoreCase("quit")){
+                    out.write(outMsg + "\n" );
+                    out.flush();
                     System.out.println("Disconnected");
                     break;
                 }
-
-                System.out.println("Client : "+ inMsg);
-                System.out.print("Send: ");
-                String outMsg = sc.nextLine();
                 out.write(outMsg+"\n");
                 out.flush();
-
+                String inMsg = in.readLine();
+                System.out.println("Server : "+ inMsg);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -42,7 +41,7 @@ public class ServerEx {
                 in.close();
                 out.close();
                 socket.close();
-                server.close();
+
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
